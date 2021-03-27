c20a1d6f614a805615bbfde67279009520c3a3cb
DudeBro249 <appdevdeploy@gmail.com>
2021-03-27 21:50:40 +0530

    added testing and finished ObservableProvider

--- react-providerx/src/providers/observableProvider.ts
+++ react-providerx/src/providers/observableProvider.ts
@@ -1,18 +1,28 @@
-import { BehaviorSubject, Observable } from 'rxjs'
+import { BehaviorSubject, from, Observable } from 'rxjs'
 
 export class ObservableProvider<T> {
     behaviorSubject$: BehaviorSubject<T> | BehaviorSubject<null>
-    observable$: Observable<T>
-    constructor(observableCreator: () => Observable<T>) {
-        this.observable$ = observableCreator()
+    observableCreator: () => (Observable<T> | null)
+    observable$?: (Observable<T> | null)
+    constructor(observableCreator: () => (Observable<T> | null)) {
+        this.observableCreator = observableCreator
         this.behaviorSubject$ = new BehaviorSubject(null)
-        if(this.observable$ === null) {
-            throw 'observableCreator cannot return null. It must return an instance of Observable'
-        }
         this._compute()
     }
     
+    get value() {
+        return this.behaviorSubject$.value
+    }
+    
+    static fromPromise<S>(promise: () => Promise<S>): ObservableProvider<S> {
+        return new ObservableProvider<S>(() => (from(promise()) as any))
+    }
+    
     _compute = () => {
+        this.observable$ = this.observableCreator()
+        if(this.observable$ === null) {
+            throw 'observableCreator cannot return null. It must return an instance of Observable'
+        }
         this.observable$.subscribe((val: T) => {
             this.behaviorSubject$.next(val as any)
         })
--- tests/react-providerx-test-app/src/App.tsx
+++ tests/react-providerx-test-app/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ObservableProvider, refresh } from 'react-providerx'
+import { from } from 'rxjs';
+import { useProvider } from 'react-providerx'
 
-function App() {
+const userProvider = new ObservableProvider(() => {
+  const fetchData = async (url: string): Promise<any> => {
+    const response = await fetch(url)
+    const json = await response.json()
+    return json
+  }
+  
+  const randomUserId = Math.floor(Math.random() * 11) + 1
+  const user$ = from(fetchData(`https://jsonplaceholder.typicode.com/users/${randomUserId}`))
+  return user$
+});
+
+
+
+const App: React.FC = () => {
+  const { isLoading, data } = useProvider(userProvider)
+  if (isLoading) {
+    return (
+      <div className="App">
+        Loading...
+      </div>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.username}
+      <button onClick={() => refresh(userProvider)}>
+        Click me to refresh user data
+      </button>
     </div>
   );
-}
+};
 
 export default App;
